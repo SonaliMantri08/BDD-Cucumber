22027054f19271d11746238d0a7fa3b47386daf5
Sonali <sonali_mantri08@yahoo.co.in>
2020-04-17 15:09:03 +1000

    Adding Feature file and Step Definition file for Data Table with Header,

--- src/main/java/features/signInWithDataTable.feature
+++ src/main/java/features/signInWithDataTable.feature
@@ -5,8 +5,8 @@ Scenario: Validating user login feature with valid credentials
 Given User comes on the landing page
 And User clicks the Sign in link
 Then User is navigated to the sign in page 
-When User enter email and password
+When User types credentials email and password
 | kom_mariee@mailinator.com | kom_mariee |
 When User unselect checkbox Stay signed in 
 When User clicks Sign In button
-Then User is navigated to the home page
+Then User is navigated to website home page
--- src/main/java/stepDefinition/SignInStepDefinitionForDataTableWithHeader.java
+++ src/main/java/stepDefinition/SignInStepDefinitionForDataTableWithHeader.java
@@ -1,6 +1,7 @@
 package stepDefinition;
 
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -11,61 +12,66 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class StepDefinitionForDataTable {
-
+public class SignInStepDefinitionForDataTableWithHeader {
+	
 	public static WebDriver driver;
 	public static WebDriverWait wait;
-
-	@Given("^User comes on the landing page$")
-	public void user_comes_on_the_landing_page() {
+	
+	@Given("^User is on the website landing page$")
+	public void user_is_on_the_website_landing_page() {
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://au.indeed.com/");
 		System.out.println("User is on landing page");
 	}
+	
 
-	@Given("^User clicks the Sign in link$")
-	public void user_clicks_the_Sign_in_link() {
+	@Given("^User clicked Sign in link$")
+	public void user_clicked_Sign_in_link()  {
 		driver.findElement(By.className("gnav-LoggedOutAccountLink-text")).click();
 		System.out.println("User clicked on sign in link");
 	}
 
-	@Then("^User is navigated to the sign in page$")
-	public void user_is_navigated_to_the_sign_in_page() {
+	@Then("^User is redirected to sign in page$")
+	public void user_is_redirected_to_sign_in_page() {
 		String page_title = driver.getTitle();
 		if (page_title.equalsIgnoreCase("Sign In | Indeed Accounts")) {
 			System.out.println("User navigated to sign in page");
 		}
 	}
 
-	@When("^User types credentials email and password$")
-	public void user_types_credentials_email_and_password(DataTable arg1) {
-		List<List<String>> data = arg1.raw();
-		driver.findElement(By.id("login-email-input")).sendKeys(data.get(0).get(0));
-		System.out.println("Email id is entered: " + data.get(0).get(0));
-		driver.findElement(By.id("login-password-input")).sendKeys(data.get(0).get(1));
-		System.out.println("Email id is entered: " + data.get(0).get(1));
+	@When("^User types in email and password$")
+	public void user_types_in_email_and_password(DataTable credentials){
+		
+		List<Map<String, String>> data=credentials.asMaps(String.class, String.class);
+		driver.findElement(By.id("login-email-input")).sendKeys(data.get(0).get("email"));
+		System.out.println("Email id is entered: " + data.get(0).get("email"));
+		
+		driver.findElement(By.id("login-password-input")).sendKeys(data.get(0).get("password"));
+		System.out.println("Email id is entered: " + data.get(0).get("password"));
+		
 	}
 
-	@When("^User unselect checkbox Stay signed in$")
-	public void user_unselect_checkbox_Stay_signed_in() {
+	@When("^User unselect the checkbox for Stay signed in$")
+	public void user_unselect_the_checkbox_for_Stay_signed_in() {
 		driver.findElement(By.cssSelector("input[id='login-rememberme-checkbox']")).click();
 		System.out.println("Checkbox unselected");
 	}
 
-	@When("^User clicks Sign In button$")
-	public void user_clicks_Sign_In_button() {
+	@When("^User click at the Sign In button$")
+	public void user_click_at_the_Sign_In_button() {
 		driver.findElement(By.id("login-submit-button")).click();
 		System.out.println("User clicked on sign in link");
 	}
 
-	@Then("^User is navigated to website home page$")
-	public void user_is_navigated_to_website_home_page() {
+	@Then("^User is redirected to home page$")
+	public void user_is_redirected_to_home_page() {
 		wait = new WebDriverWait(driver, 5);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div[aria-label='Toggle menu']")));
 		String title = driver.getTitle();
@@ -73,5 +79,4 @@ public class StepDefinitionForDataTable {
 		System.out.println("Successful login");
 		driver.quit();
 	}
-
 }
