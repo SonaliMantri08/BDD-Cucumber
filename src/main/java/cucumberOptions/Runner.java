31a971b7a5471b6c6c939a221d01490c17afc413
Sonali <sonali_mantri08@yahoo.co.in>
2020-04-20 11:00:01 +1000

    Updated Runner file

--- src/main/java/cucumberOptions/Runner.java
+++ src/main/java/cucumberOptions/Runner.java
@@ -7,13 +7,16 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="src/main/java/features/signInWithExamplesKeyword.feature",
+		features="src/main/java/features/signInUsingMaps.feature",
 		glue= {"stepDefinition"},
 		plugin= {"pretty","html:test-output"},
 		monochrome=true,
-		dryRun=true
+		dryRun=false
 		)
 
 public class Runner {
-//features="src/main/java/features/signIn.feature"
+//features="src/main/java/features/signIn.feature" 
+	//signInWithExamplesKeyword.feature
+	//features="src/main/java/features/signInWithDataTable.feature"
+	//features="src/main/java/features/signInWithDataTableWithHeader.feature"
 }
